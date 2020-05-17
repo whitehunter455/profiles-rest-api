de4c643478264497172b305d3833d35d66b0e996
whitehunter455 <setiawankemiko13@outlook.com>
2020-05-17 15:36:35 +0700

    ViewSet to Profiles API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-
 from profiles_api import views
 
+#Section ViewSet
+from django.urls import include
+from rest_framework.routers import DefaultRouter
+
+#Created for ViewSet
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+#===========================
 
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view()),
+    path('hello-view/', views.HelloApiView.as_view()), #http://127.0.0.1:8000/api/hello-view/
+    path('', include(router.urls)), #http://127.0.0.1:8000/api/
 ]
